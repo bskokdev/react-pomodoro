41d242f63debd5cb0968522c86902d745638a0d6
Boris Skok <skokcmd@gmail.com>
2023-05-05 22:13:56 +0200

    updated toggle docs

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -7,8 +7,8 @@ interface IToggleProps {
 
 /**
  * This component renders an arrow up or down depending on the toggleValue and calls the toggle function on click
- * @param toggleValue
- * @param toggle
+ * @param toggleValue is the toggle activated - true or false
+ * @param toggle function to be called upon toggle
  * @constructor
  */
 export function Toggle({ toggleValue, toggle }: IToggleProps) {
