1f886f70985581635f443a03b087365ddcbae192
Boris Skok <skokcmd@gmail.com>
2023-05-05 22:30:29 +0200

    set initial toggle value to true

--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -4,7 +4,7 @@ import {useCallback, useState} from "react";
  * A hook that returns a boolean value and a function to toggle it.
  * @param initialValue
  */
-export function useToggle(initialValue = false): [boolean, () => void] {
+export function useToggle(initialValue = true): [boolean, () => void] {
   const [toggleValue, setToggleValue] = useState(initialValue);
   const toggle = useCallback(() => setToggleValue((value) => !value), []);
   return [toggleValue, toggle];
